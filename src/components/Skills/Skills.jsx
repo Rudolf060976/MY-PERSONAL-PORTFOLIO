dc22b7796260769ce70d26c1d19977a0b749ca0c
Rafael Urbina <rafaelurbinan@hotmail.com>
2020-05-25 18:52:14 -0400

    adjusting Skills Section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 import { graphql, useStaticQuery } from 'gatsby';
 import BackgroundImage from 'gatsby-background-image';
 import SkillsContent from './SkillsContent';
-import { Parallax } from 'react-scroll-parallax';
 
-const BackgroundSection = ({ className, children }) => {
+
+const BackgroundSection = ({ className, children, id }) => {
 
     const data = useStaticQuery(graphql`
         query {
@@ -22,246 +22,76 @@ const BackgroundSection = ({ className, children }) => {
     const imageData = data.desktop.childImageSharp.fluid;
 
     return (
-        <BackgroundImage Tag="div" className={className} fluid={imageData} backgroundColor={'#325A73'}>
+        <BackgroundImage Tag="div" className={className} fluid={imageData} backgroundColor={'#325A73'} id={id}>
             {children}
         </BackgroundImage>
     );
 };
 
-const StyledBackgroundSection = styled(BackgroundSection)`
+const StyledContainer = styled(BackgroundSection)`
 
-    width: 100%;
+    width: 100%;    
 
-    height: 150vh; 
+    background-position: center;
 
-    min-height: 150vh; 
+    background-size: cover;
 
-    position: absolute;
+    background-attachment: fixed;
+
+    height: 1800px; 
+
+    position: relative;
 
     z-index: -5000;
   
 /* RECUERDEN QUE CUANDO SE USA MAX-WIDTH LOS VALORES DEBEN SER DESCENDENTES, Y CON MIN-WIDTH ASCENDENTES */
-
      
+    @media (max-width: 1250px) {
 
-     @media (max-width: 1000px) and (max-height: 700px) {
-
-        height: 360vh;
-
-        min-height: 360vh; 
-        
-
-    }
-     @media (max-width: 450px) and (max-height: 900px) {
-
-        height: 350vh;
-
-        min-height: 350vh; 
+        height: 2000px;       
 
     }
 
-    @media (max-width: 450px) and (max-height: 800px) {
+     @media (max-width: 830px) {
 
-        height: 370vh;
-
-        min-height: 370vh; 
+        height: 2250px;       
 
     }
 
+     @media (max-width: 680px) {
 
-     @media (max-width: 450px) and (max-height: 750px) {
-
-        height: 390vh;
-
-        min-height: 390vh; 
+        height: 2550px;       
 
     }
 
-     @media (max-width: 400px) and (max-height: 850px) {
+     @media (max-width: 530px) {
 
-        height: 285vh;
-
-        min-height: 285vh; 
+        height: 3000px;       
 
     }
 
-     @media (max-width: 400px) and (max-height: 700px) {
+     @media (max-width: 410px) {
 
-        height: 360vh;
-
-        min-height: 360vh; 
+        height: 2400px;       
 
     }
 
-     @media (max-width: 370px) and (max-height: 650px) {
+     @media (max-width: 350px) {
 
-        height: 410vh;
-
-        min-height: 410vh; 
+        height: 2800px;       
 
     }
-
-     @media (max-width: 350px) and (max-height: 650px) {
-
-        height: 480vh;
-
-        min-height: 480vh; 
-
-    }
-
-
-
-
-
-     @media (min-width: 700px) and (min-height: 1000px)  {
-
-        height: 220vh;
-
-        min-height: 220vh; 
-
-    }
-
-     @media (min-width: 750px) and (min-height: 1100px)  {
-
-        height: 180vh;
-
-        min-height: 180vh; 
-
-    }
-
-     /* PARA ANCHOS MAYORES A 1000px */
-
-    @media (min-width: 1000px) and (min-height: 700px)  {
-
-        height: 260vh;
-
-        min-height: 260vh; 
-
-    }
-
-      @media (min-width: 1000px) and (min-height: 1000px)  {
-
-        height: 170vh;
-
-        min-height: 170vh; 
-
-    }
-
-     @media (min-width: 1000px) and (min-height: 1200px)  {
-
-        height: 160vh;
-
-        min-height: 160vh; 
-
-    }
-
-     @media (min-width: 1000px) and (min-height: 1300px)  {
-
-        height: 150vh;
-
-        min-height: 150vh; 
-
-    }
-    
-
-     /* PARA ANCHOS MAYORES A 1200px */
-
-      @media (min-width: 1200px) and (min-height: 600px)  {
-
-        height: 240vh;
-
-        min-height: 240vh; 
-
-    }
-
-     @media (min-width: 1200px) and (min-height: 700px)  {
-
-        height: 220vh;
-
-        min-height: 220vh; 
-
-    }
-
-       /* PARA ANCHOS MAYORES A 1300px */
-
-     @media (min-width: 1300px) and (min-height: 700px) {
-
-        height: 220vh;
-
-        min-height: 220vh; 
-
-    }
-
-
-    @media (min-width: 1300px) and (min-height: 900px)  {
-
-        height: 190vh;
-
-        min-height: 190vh; 
-
-    }
-
-     @media (min-width: 1300px) and (min-height: 1000px)  {
-
-        height: 170vh;
-
-        min-height: 170vh; 
-
-    }
-
-     @media (min-width: 1300px) and (min-height: 1100px)  {
-
-        height: 150vh;
-
-        min-height: 150vh; 
-
-    }
-
-
-    
-
-   
-   
-
-    
-
-
-    
-    
-
-    
-
-    
-   
 
 `;
 
-const ParallaxImage = () => (
-    <Parallax className="custom-class" y={[-50, 50]} tagOuter="div">
-        <StyledBackgroundSection />
-    </Parallax>
-);
 
 
-const StyledContainer = styled.section`
-
-    width: 100%;  
-    
-    position: relative;
-
-    overflow: hidden;
-
-    z-index: -5000;
-    
-    
-`;
 
 
 function Skills() {
     return (
-        <StyledContainer id="skills-scroll-point">
-            <ParallaxImage />  
-            <SkillsContent />          
+        <StyledContainer id="skills-scroll-point">            
+                <SkillsContent />                          
         </StyledContainer>
     );
 }
